dc49f914fee02ea7d896655e39100e822f2f0852
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-04 21:13:50 +0100

    Add method: initQueueAsync

--- SessionQueue/Program.cs
+++ SessionQueue/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus.Management;
 
 namespace SessionQueue
 {
   public class Program
   {
-    public static void Main(string[] args)
+    private static readonly string _connString = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_ServiceBus");
+
+    private const string _queueName = "session-queue";
+
+    public static async Task Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      try
+      {
+        await initQueueAsync();
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+
+    private static async Task initQueueAsync()
+    {
+      var managementClient = new ManagementClient(_connString);
+
+      if (await managementClient.QueueExistsAsync(_queueName))
+        await managementClient.DeleteQueueAsync(_queueName);
+
+      var queueDescription = new QueueDescription(_queueName) { RequiresSession = true };
+
+      await managementClient.CreateQueueAsync(queueDescription);
+
+      await managementClient.CloseAsync();
     }
   }
 }
