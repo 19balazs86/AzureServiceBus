a366cba77682d0e48628b902029b328fbda27061
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-04 21:14:11 +0100

    Add method: sendMessagesAsync.

--- SessionQueue/Program.cs
+++ SessionQueue/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
 using Microsoft.Azure.ServiceBus.Management;
 
 namespace SessionQueue
@@ -10,16 +13,41 @@ namespace SessionQueue
 
     private const string _queueName = "session-queue";
 
+    private const int _lineCount = 10, _lineSize = 20;
+
     public static async Task Main(string[] args)
     {
       try
       {
         await initQueueAsync();
+
+        await sendMessagesAsync();
       }
       catch (Exception ex)
       {
         Console.WriteLine(ex.Message);
       }
+    }
+
+    private static async Task sendMessagesAsync()
+    {
+      var messageSender = new MessageSender(_connString, _queueName);
+
+      for (int i = 0; i < _lineCount; i++)
+      {
+        string sessionId = $"Line-{i}";
+
+        for (int j = 0; j < _lineSize; j++)
+        {
+          byte[] body = Encoding.UTF8.GetBytes($"Pos-{j}");
+
+          Message message = new Message(body) { SessionId = sessionId };
+
+          await messageSender.SendAsync(message);
+        }
+      }
+
+      await messageSender.CloseAsync();
     }
 
     private static async Task initQueueAsync()
