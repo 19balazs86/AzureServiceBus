3e05c2a8df68cd7d3dab4436ea2dbc749fde0385
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-13 10:44:01 +0100

    Add PowerShell command to create resources.

---
+++ Azure-Commands.ps1
@@ -0,0 +1,39 @@
+﻿# Install the Azure PowerShell module
+# https://docs.microsoft.com/en-us/powershell/azure/install-az-ps
+
+# Sign in
+# Connect-AzAccount
+
+param(
+  [string]
+  # [Parameter(Mandatory=$true)]
+  $resGroupName = "test-ResGroup",
+
+  [string] $location = "North Europe",
+
+  [string] $serviceBusName = "balazs-test-ServBus",
+
+  [string] $sku = "Standard"
+)
+
+Get-AzResourceGroup -Name $resGroupName -Location $location -ErrorVariable notPresent -ErrorAction SilentlyContinue | Out-Null
+
+if ($notPresent)
+{
+  New-AzResourceGroup -Name $resGroupName -Location $location
+}
+
+Get-AzServiceBusNamespace `
+  -ResourceGroupName $resGroupName `
+  -NamespaceName $serviceBusName `
+  -ErrorVariable notPresent `
+  -ErrorAction SilentlyContinue | Out-Null
+
+if ($notPresent)
+{
+  New-AzResourceGroup -Name $resGroupName -Location $location
+}
+
+$keys = Get-AzServiceBusKey -ResourceGroupName $resGroupName -Namespace $serviceBusName -Name "RootManageSharedAccessKey"
+
+Write-Host "PrimaryConnString = " $keys.PrimaryConnectionString
